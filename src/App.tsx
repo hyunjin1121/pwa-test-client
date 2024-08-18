31ea292c8ec50ae56f0676414ee51e169f8150be
hyun <okmt0423@gmail.com>
2024-08-18 21:40:13 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('Service Worker Registered');
+      });
+    }
+  }, []);
+
+  const subscribeToPush = async () => {
+    const registration = await navigator.serviceWorker.ready;
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: 'BLMuqANzgt98IBfsEIeO1a7ZMhtKV73qqxWvn44oRhxamelcLU3jzEPXrXR9S_qy8JwfzeJzKhgCX8cvHNB7QG4', // VAPID 공개 키
+    });
+
+    console.log('Subscription:', subscription);
+
+    await fetch('https://a2b5780289322ce17a2fac7c4bf9e221.serveo.net/subscribe', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(subscription),
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web Push Notifications</h1>
+      <button onClick={subscribeToPush}>Subscribe to Push</button>
     </div>
   );
-}
+};
 
 export default App;
